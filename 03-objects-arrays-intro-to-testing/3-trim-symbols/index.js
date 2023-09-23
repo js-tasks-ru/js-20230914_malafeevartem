105d99bba13bff1ec7dce1a9a7ad736645acc236
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-09-23 17:46:56 +0300

    complete

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,32 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (!size) {
+    return string;
+  }
+
+  const totalSymbols = [];
+
+  let counter;
+  let activeSymbol;
+
+  for (const symbol of string) {
+    if (symbol !== activeSymbol) {
+      counter = 0;
+      activeSymbol = symbol;
+    }
+
+    if (counter >= size) {
+      continue;
+    }
+
+    counter += 1;
+    totalSymbols.push(activeSymbol);
+  }
+
+  return totalSymbols.join('');
 }

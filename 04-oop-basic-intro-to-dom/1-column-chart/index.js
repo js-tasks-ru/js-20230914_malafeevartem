95c875d94a1c293175c97dc00b78d02301acef49
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-09-28 10:34:16 +0300

    lint

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,66 +1,66 @@
 export default class ColumnChart {
-    constructor({
-        data = [],
-        label = null,
-        value = null,
-        link = null,
-        formatHeading = null,
-    } = {}) {
-        this.data = data,
-            this.label = label,
-            this.value = value,
-            this.link = link,
-            this.formatHeading = formatHeading,
-            this.chartHeight = 50;
-        this.element = this.createChart();
+  constructor({
+    data = [],
+    label = null,
+    value = null,
+    link = null,
+    formatHeading = null,
+  } = {}) {
+    this.data = data,
+    this.label = label,
+    this.value = value,
+    this.link = link,
+    this.formatHeading = formatHeading,
+    this.chartHeight = 50;
+    this.element = this.createChart();
+  }
+
+  getLabel() {
+    return this.label;
+  }
+
+  getTitle() {
+    return `Total ${this.label}`;
+  }
+
+  getLink() {
+    return this.link;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  getFormatHeading() {
+    return this.formatHeading;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  getNormalizedValue() {
+    const currentValue = new Intl.NumberFormat('en-EN').format(this.getValue());
+    return (this.getFormatHeading())
+      ? this.getFormatHeading()(currentValue)
+      : currentValue;
+  }
+
+  createChart() {
+    const chartElement = document.createElement('div');
+    chartElement.classList.add('column-chart', 'column-chart_loading');
+    chartElement.setAttribute('style', `--chart-height: ${this.chartHeight}`);
+    chartElement.innerHTML = this.getTemplate();
+
+    if (this.getData().length !== 0) {
+      chartElement.classList.remove('column-chart_loading');
     }
 
-    getLabel() {
-        return this.label;
-    }
+    return chartElement;
+  }
 
-    getTitle() {
-        return `Total ${this.label}`
-    }
-
-    getLink() {
-        return this.link;
-    }
-
-    getValue() {
-        return this.value;
-    }
-
-    getFormatHeading() {
-        return this.formatHeading;
-    }
-
-    getData() {
-        return this.data;
-    }
-
-    getNormalizedValue() {
-        const currentValue = new Intl.NumberFormat('en-EN').format(this.getValue());
-        return (this.getFormatHeading())
-            ? this.getFormatHeading()(currentValue)
-            : currentValue;
-    }
-
-    createChart() {
-        const chartElement = document.createElement('div');
-        chartElement.classList.add('column-chart', 'column-chart_loading');
-        chartElement.setAttribute('style', `--chart-height: ${this.chartHeight}`);
-        chartElement.innerHTML = this.getTemplate();
-
-        if (this.getData().length !== 0) {
-            chartElement.classList.remove('column-chart_loading');
-        }
-
-        return chartElement;
-    }
-
-    getTemplate() {
-        return `
+  getTemplate() {
+    return `
             <div class="column-chart__title">
                 ${this.getTitle()}
                 ${this.getLinkTemplate()}
@@ -74,48 +74,48 @@ export default class ColumnChart {
                 </div>
             </div>
         `;
-    }
+  }
 
-    getLinkTemplate() {
-        if (this.getLink()) {
-            return `
+  getLinkTemplate() {
+    if (this.getLink()) {
+      return `
                 <a class="column-chart__link" href="${this.getLink()}">
                     View all
                 </a>
             `;
-        }
-
-        return '';
     }
 
-    getGraphTemplate() {
-        const maxValue = Math.max(...this.getData());
-        const columns = this.getData().map((value) => {
-            const pixels = Math.floor((value * 50) / maxValue);
-            const percent = Math.round((value / maxValue) * 100);
-            return this.getColumnTemplate(pixels, percent);
-        });
+    return '';
+  }
 
-        return columns.join('');
-    }
+  getGraphTemplate() {
+    const maxValue = Math.max(...this.getData());
+    const columns = this.getData().map((value) => {
+      const pixels = Math.floor((value * 50) / maxValue);
+      const percent = Math.round((value / maxValue) * 100);
+      return this.getColumnTemplate(pixels, percent);
+    });
 
-    getColumnTemplate(pixels, percent) {
-        return `
+    return columns.join('');
+  }
+
+  getColumnTemplate(pixels, percent) {
+    return `
             <div style="--value: ${pixels}" data-tooltip="${percent}%"></div>
         `;
-    }
+  }
 
-    remove() {
-        this.element?.remove();
-        this.element = null;
-    }
+  remove() {
+    this.element.remove();
+    this.element = null;
+  }
 
-    destroy() {
-        this.remove();
-    }
+  destroy() {
+    this.remove();
+  }
 
-    update() {
-        const body = document.querySelector('div[data-element="body"]');
-        body.innerHTML = this.getGraphTemplate();
-    }
+  update() {
+    const body = document.querySelector('div[data-element="body"]');
+    body.innerHTML = this.getGraphTemplate();
+  }
 }

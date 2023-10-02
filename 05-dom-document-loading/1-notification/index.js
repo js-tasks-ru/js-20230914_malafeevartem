ee29f2cc6b117d6f17e7c1f18d255d6d42c1bb61
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-10-02 16:41:20 +0300

    complete notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,90 @@
-export default class NotificationMessage {
+const defaultProperties = {
+    duration: 2000,
+    type: 'success',
+};
 
+export default class NotificationMessage {
+    static activeNotification = null;
+
+    constructor(message, properties = {}) {
+        properties = { ...defaultProperties, ...properties };
+        this._message = message;
+        this._duration = properties.duration;
+        this._type = properties.type;
+        this._element = this.createNotificationElement();
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    get duration() {
+        return this._duration;
+    }
+
+    get message() {
+        return this._message;
+    }
+
+    get element() {
+        return this._element;
+    }
+
+    set element(element) {
+        this._element = element;
+    }
+
+    get template() {
+        return `
+            <div class="notification ${this.type}" style="--value:${this.convertedDurationToSeconds()}s">
+                <div class="timer"></div>
+                    <div class="inner-wrapper">
+                        <div class="notification-header">${this.type}</div>
+                        <div class="notification-body">
+                            ${this.message}
+                        </div>
+                </div>
+            </div>
+        `;
+    };
+
+    convertedDurationToSeconds() {
+        return this.duration / 1000;
+    }
+
+    createNotificationElement() {
+        const element = document.createElement('div');
+        element.insertAdjacentHTML('afterbegin', this.template);
+
+        return element.firstElementChild;
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    reset() {
+        this.remove();
+        clearTimeout(NotificationMessage.activeNotification?.timeoutID);
+        NotificationMessage.activeNotification = null;
+    }
+
+    destroy() {
+        this.reset();
+    }
+
+    show(target = document.body) {
+        if (NotificationMessage.activeNotification) {
+            this.reset();
+        }
+
+        NotificationMessage.activeNotification = {
+            element: this.element,
+            timeoutID: setTimeout(() => {
+                this.reset();
+            }, this.duration),
+        };
+
+        target.append(NotificationMessage.activeNotification.element);
+    }
 }

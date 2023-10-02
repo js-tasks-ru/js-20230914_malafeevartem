ef5e3eb119adc2116e2dfb409cd8b80a82940bb1
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-10-02 16:46:16 +0300

    lint notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,41 +1,41 @@
 const defaultProperties = {
-    duration: 2000,
-    type: 'success',
+  duration: 2000,
+  type: 'success',
 };
 
 export default class NotificationMessage {
     static activeNotification = null;
 
     constructor(message, properties = {}) {
-        properties = { ...defaultProperties, ...properties };
-        this._message = message;
-        this._duration = properties.duration;
-        this._type = properties.type;
-        this._element = this.createNotificationElement();
+      properties = { ...defaultProperties, ...properties };
+      this._message = message;
+      this._duration = properties.duration;
+      this._type = properties.type;
+      this._element = this.createNotificationElement();
     }
 
     get type() {
-        return this._type;
+      return this._type;
     }
 
     get duration() {
-        return this._duration;
+      return this._duration;
     }
 
     get message() {
-        return this._message;
+      return this._message;
     }
 
     get element() {
-        return this._element;
+      return this._element;
     }
 
     set element(element) {
-        this._element = element;
+      this._element = element;
     }
 
     get template() {
-        return `
+      return `
             <div class="notification ${this.type}" style="--value:${this.convertedDurationToSeconds()}s">
                 <div class="timer"></div>
                     <div class="inner-wrapper">
@@ -46,45 +46,45 @@ export default class NotificationMessage {
                 </div>
             </div>
         `;
-    };
+    }
 
     convertedDurationToSeconds() {
-        return this.duration / 1000;
+      return this.duration / 1000;
     }
 
     createNotificationElement() {
-        const element = document.createElement('div');
-        element.insertAdjacentHTML('afterbegin', this.template);
+      const element = document.createElement('div');
+      element.insertAdjacentHTML('afterbegin', this.template);
 
-        return element.firstElementChild;
+      return element.firstElementChild;
     }
 
     remove() {
-        this.element.remove();
+      this.element.remove();
     }
 
     reset() {
-        this.remove();
-        clearTimeout(NotificationMessage.activeNotification?.timeoutID);
-        NotificationMessage.activeNotification = null;
+      this.remove();
+      clearTimeout(NotificationMessage.activeNotification?.timeoutID);
+      NotificationMessage.activeNotification = null;
     }
 
     destroy() {
-        this.reset();
+      this.reset();
     }
 
     show(target = document.body) {
-        if (NotificationMessage.activeNotification) {
-            this.reset();
-        }
+      if (NotificationMessage.activeNotification) {
+        this.reset();
+      }
 
-        NotificationMessage.activeNotification = {
-            element: this.element,
-            timeoutID: setTimeout(() => {
-                this.reset();
-            }, this.duration),
-        };
+      NotificationMessage.activeNotification = {
+        element: this.element,
+        timeoutID: setTimeout(() => {
+          this.reset();
+        }, this.duration),
+      };
 
-        target.append(NotificationMessage.activeNotification.element);
+      target.append(NotificationMessage.activeNotification.element);
     }
 }

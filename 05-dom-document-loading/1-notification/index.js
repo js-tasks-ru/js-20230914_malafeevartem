b1f5449b901bc9810128607ff0f87fc902e7e170
Artem Malafeev <ma.evgenyevich@gmail.com>
2023-10-03 09:13:58 +0300

    fix

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -4,38 +4,48 @@ const defaultProperties = {
 };
 
 export default class NotificationMessage {
-    static activeNotification = null;
+  static activeNotification = null;
+  _timeoutID = null;
+  _element = null;
 
-    constructor(message, properties = {}) {
-      properties = { ...defaultProperties, ...properties };
-      this._message = message;
-      this._duration = properties.duration;
-      this._type = properties.type;
-      this._element = this.createNotificationElement();
-    }
+  constructor(message, properties = {}) {
+    properties = { ...defaultProperties, ...properties };
+    this._message = message;
+    this._duration = properties.duration;
+    this._type = properties.type;
+    this._element = this.createNotificationElement();
+  }
 
-    get type() {
-      return this._type;
-    }
+  get type() {
+    return this._type;
+  }
 
-    get duration() {
-      return this._duration;
-    }
+  get duration() {
+    return this._duration;
+  }
 
-    get message() {
-      return this._message;
-    }
+  get message() {
+    return this._message;
+  }
 
-    get element() {
-      return this._element;
-    }
+  get element() {
+    return this._element;
+  }
 
-    set element(element) {
-      this._element = element;
-    }
+  set element(element) {
+    this._element = element;
+  }
 
-    get template() {
-      return `
+  get timeoutID() {
+    return this._timeoutID;
+  }
+
+  set timeoutID(id) {
+    this._timeoutID = id;
+  }
+
+  get template() {
+    return `
             <div class="notification ${this.type}" style="--value:${this.convertedDurationToSeconds()}s">
                 <div class="timer"></div>
                     <div class="inner-wrapper">
@@ -46,47 +56,43 @@ export default class NotificationMessage {
                 </div>
             </div>
         `;
+  }
+
+  convertedDurationToSeconds() {
+    return this.duration / 1000;
+  }
+
+  createNotificationElement() {
+    const element = document.createElement('div');
+    element.insertAdjacentHTML('afterbegin', this.template);
+    return element.firstElementChild;
+  }
+
+  createTimer() {
+    return setTimeout(() => this.destroy(), this.duration);
+  }
+
+  destroy() {
+    this.remove();
+    this.removeTimer();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  removeTimer() {
+    clearTimeout(this.timeoutID);
+    this.timeoutID = null;
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.destroy();
     }
 
-    convertedDurationToSeconds() {
-      return this.duration / 1000;
-    }
-
-    createNotificationElement() {
-      const element = document.createElement('div');
-      element.insertAdjacentHTML('afterbegin', this.template);
-
-      return element.firstElementChild;
-    }
-
-    remove() {
-      this.element.remove();
-    }
-
-    resetActive() {
-      this.remove();
-        NotificationMessage.activeNotification?.element.remove();
-        clearTimeout(NotificationMessage.activeNotification?.timeoutID);
-        NotificationMessage.activeNotification = null;
-    }
-
-    destroy() {
-      this.remove();
-      this.resetActive();
-    }
-
-    show(target = document.body) {
-      if (NotificationMessage.activeNotification) {
-        this.resetActive();
-      }
-
-      NotificationMessage.activeNotification = {
-        element: this.element,
-        timeoutID: setTimeout(() => {
-          this.resetActive();
-        }, this.duration),
-      };
-
-      target.append(NotificationMessage.activeNotification.element);
-    }
+    NotificationMessage.activeNotification = this;
+    this.timeoutID = this.createTimer();
+    target.append(this.element);
+  }
 }
